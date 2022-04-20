0aa8aa5067f64afe709a88b7b3bc6cfebb32f715
Carlos Menezes <carlos-menezes212@hotmail.com>
2022-04-20 01:25:37 +0100

    Initial implementation of the `Main` class

---
+++ src/main/java/client/Main.java
@@ -0,0 +1,10 @@
+package client;
+
+import picocli.CommandLine;
+
+public class Main {
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new Client()).execute(args);
+        System.exit(exitCode);
+    }
+}
