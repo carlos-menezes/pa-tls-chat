96c8f620fd27014b7f8d8838e60489105afd7964
Carlos Menezes <carlos-menezes212@hotmail.com>
2022-04-25 12:04:49 +0100

    Add missing validators do `populateValidators` method

--- src/main/java/shared/encryption/validator/EncryptionValidator.java
+++ src/main/java/shared/encryption/validator/EncryptionValidator.java
@@ -6,13 +6,13 @@ import shared.encryption.validator.exceptions.InvalidKeySizeException;
 import java.util.HashMap;
 import java.util.List;
 
-public class Validator {
+public class EncryptionValidator {
     private final List<String> supportedAlgorithms;
     private final List<Integer> supportedKeySizes;
 
     private final HashMap<String, Validatable> validators;
 
-    public Validator(List<String> supportedAlgorithms, List<Integer> supportedKeySizes) throws
+    public EncryptionValidator(List<String> supportedAlgorithms, List<Integer> supportedKeySizes) throws
             InvalidEncryptionAlgorithmException, InvalidKeySizeException {
         this.supportedAlgorithms = supportedAlgorithms;
         this.supportedKeySizes = supportedKeySizes;
@@ -24,6 +24,8 @@ public class Validator {
     private void populateValidators() {
         this.validators.put("DES", new DESValidator());
         this.validators.put("AES", new AESValidator());
+        this.validators.put("RSA", new RSAValidator());
+        this.validators.put("3DES", new ThreeDESValidator());
     }
 
     public void validate() throws InvalidEncryptionAlgorithmException, InvalidKeySizeException {
