8e9f142eca5639d6777edb81b835beb30b3b86f7
Carlos Menezes <carlos-menezes212@hotmail.com>
2022-05-05 10:49:14 +0100

    Add helper method to check if an hashing algorithm is supported by the JVM

--- src/main/java/shared/hashing/validator/HashingValidator.java
+++ src/main/java/shared/hashing/validator/HashingValidator.java
@@ -1,7 +1,10 @@
 package shared.hashing.validator;
 
 import shared.hashing.validator.exceptions.InvalidHashingAlgorithmException;
+import shared.hashing.validator.exceptions.UnsupportedHashingAlgorithmException;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 /**
@@ -24,9 +27,26 @@ public class HashingValidator {
      *
      * @param algorithm algorithm's name
      */
-    public void validate(String algorithm) {
+    public void validate(String algorithm) throws InvalidHashingAlgorithmException, UnsupportedHashingAlgorithmException {
         if (!this.validAlgorithms.contains(algorithm)) {
             throw new InvalidHashingAlgorithmException(algorithm, validAlgorithms);
+        }
+
+        if (!isHashingAlgorithmSupported(algorithm)) {
+            throw new UnsupportedHashingAlgorithmException(algorithm);
+        }
+    }
+
+    /**
+     * Checks whether a given algorithm is supported by the JVM.
+     * @return true, if it is supporte by the JVM; false, otherwise.
+     */
+    public static boolean isHashingAlgorithmSupported(String algorithm) {
+        try {
+            MessageDigest.getInstance(algorithm);
+            return true;
+        } catch (NoSuchAlgorithmException e) {
+            return false;
         }
     }
 }
